67001b11b061912f49b694b915b47b838d276342
Turbobot <turbobot@vercel.com>
2025-10-22 21:42:21 +0530

    feat(create-turbo): apply official-starter transform

--- apps/frontend/src/lib/axios.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-
-export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api",
-    withCredentials: true
-});
-
--- packages/db/src/types.ts
+++
@@ -1,3 +0,0 @@
-export enum PLatform {
-    ResendEmail = ""
-}
